ebdb1f7aec5cb465d9b03d84f9b908e8a93f640b
junly <binghe519@126.com>
2018-06-25 22:59:46 +0800

    inti commit

---
+++ parent/eureka/src/main/java/com/junly/container/EurekaserverApplication.java
@@ -0,0 +1,14 @@
+package com.junly.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaserverApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
